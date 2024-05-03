22678033f51fd9949653347db40aff70345fa514
Andrzej Czarny <aczarny1988@gmail.com>
2024-05-03 11:14:22 +0200

    Changed ChuckNorrisQuotes declaration.

--- src/main/java/aczarny/springframework/spring5jokesappv2/services/JokesServiceImpl.java
+++ src/main/java/aczarny/springframework/spring5jokesappv2/services/JokesServiceImpl.java
@@ -5,8 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokesServiceImpl implements JokesService {
+
+    private final ChuckNorrisQuotes quotes;
+
+    public JokesServiceImpl() {
+        quotes = new ChuckNorrisQuotes();
+    }
+
     public String printJoke() {
-        ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
+
        return quotes.getRandomQuote();
     }
 }
